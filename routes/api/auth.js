7d8c7c31496f6f1e788a4bbc94d99c9b061f9ef6
Cesar Gomez <zootechdrum@gmail.com>
2020-01-10 17:25:45 -0800

    added validation to post authentication route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator/check')
 
-
-//@route    GET api/auth
-//@desc     Test route
+//@route    POST api/auth
+//@desc     Register user
 //@access   Public
-router.get('/', (req,res) => res.send('Auth route'));
+router.post('/', (req,res) => {
+    console.log(req.body)
+res.send('Auth route');
+})
 
 module.exports = router;
