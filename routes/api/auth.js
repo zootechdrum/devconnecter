41202ae111386aa2c236fb925b41cbc302e728bc
Cesar Gomez <zootechdrum@gmail.com>
2020-01-10 17:29:25 -0800

    added validtion on request on post auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,7 +5,11 @@ const {check, validationResult} = require('express-validator/check')
 //@route    POST api/auth
 //@desc     Register user
 //@access   Public
-router.post('/', (req,res) => {
+router.post('/',[
+    check('name', 'Name is required').not().isEmpty().
+    check('email', "Please include a valid email").isEmail().
+    check('password','Please enter a password with 6 or more charechters')
+], (req,res) => {
     console.log(req.body)
 res.send('Auth route');
 })
