dacab6d9748a7fee020566673c82d52665f75674
Cesar Gomez <zootechdrum@gmail.com>
2020-01-12 20:49:33 -0800

    added post update route for user to save to db. no test has been done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,18 +9,18 @@ const { check, validationResult } = require('express-validator/check')
 //@route    GET api/profile/me
 //@desc     Test route
 //@access   Public
-router.get('/me', auth, async (req,res) => {
+router.get('/me', auth, async (req, res) => {
 
     try {
-        const profile = await Profile.findOne({ user: req.user.id}).populate('user', ['name','avatar']);
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
 
 
-        if(!profile) {
-            return res.status(400).json({msg: "There is no profile for this user"})
+        if (!profile) {
+            return res.status(400).json({ msg: "There is no profile for this user" })
         }
 
         res.json(profile)
-    } catch(err) {
+    } catch (err) {
         console.log(err.message);
         res.status(500).send("Server Error")
     }
@@ -29,73 +29,94 @@ router.get('/me', auth, async (req,res) => {
 
 
 router.post('/',
-    [auth,      
+    [auth,
         [
-            check('status',"Status is required").not().isEmpty(),
-            check('skills',"Skills is required").not().isEmpty()
+            check('status', "Status is required").not().isEmpty(),
+            check('skills', "Skills is required").not().isEmpty()
         ]
     ],
-   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty) {
-        return res.status(400).json({ errors: errors.array() })
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            skills,
+            youtube,
+            facebook,
+            twitter,
+            instagram,
+            linkedin
+        } = req.body;
+
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if (company) profileFields.company = company;
+        if (website) profileFields.website = website;
+        if (location) profileFields.location = location;
+        if (bio) profileFields.bio = bio;
+        if (status) profileFields.status = status;
+        if (githubusername) profileFields.githubusername = githubusername;
+        if (skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+        }
+
+        // Build social object
+        profileFields.social = {};
+        if (youtube) profileFields.social.youtube = youtube;
+        if (twitter) profileFields.social.twitter = twitter;
+        if (facebook) profileFields.social.facebook = facebook;
+        if (linkedin) profileFields.social.linkedin = linkedin;
+        if (instagram) profileFields.social.instagram = instagram; const profileFields = {};
+        profileFields.user = req.user.id;
+        if (company) profileFields.company = company;
+        if (website) profileFields.website = website;
+        if (location) profileFields.location = location;
+        if (bio) profileFields.bio = bio;
+        if (status) profileFields.status = status;
+        if (githubusername) profileFields.githubusername = githubusername;
+        if (skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+        }
+
+        // Build social object
+        profileFields.social = {};
+        if (youtube) profileFields.social.youtube = youtube;
+        if (twitter) profileFields.social.twitter = twitter;
+        if (facebook) profileFields.social.facebook = facebook;
+        if (linkedin) profileFields.social.linkedin = linkedin;
+        if (instagram) profileFields.social.instagram = instagram;
+
+        //Build profile object
+        try {
+            let profile = await Profile.findOne({ user: req.user.id })
+
+            if (profile) {
+                profile = await Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileFields },
+                    { new: true }
+                )
+                return res.json(profile)
+            }
+
+            profile = new Profile(profileFields);
+
+            await profile.save()
+            res.json(profile);
+
+        } catch (err) {
+            console.error(error)
+            res.status(500).send("server error")
+        }
     }
-    const {
-        company,
-        website,
-        location,
-        bio,
-        status,
-        githubusername,
-        skills,
-        youtube,
-        facebook,
-        twitter,
-        instagram,
-        linkedin
-    } = req.body;
-
-    const profileFields = {};
-    profileFields.user = req.user.id;
-    if (company) profileFields.company = company;
-    if (website) profileFields.website = website;
-    if (location) profileFields.location = location;
-    if (bio) profileFields.bio = bio;
-    if (status) profileFields.status = status;
-    if (githubusername) profileFields.githubusername = githubusername;
-    if (skills) {
-      profileFields.skills = skills.split(',').map(skill => skill.trim());
-    }
-
-    // Build social object
-    profileFields.social = {};
-    if (youtube) profileFields.social.youtube = youtube;
-    if (twitter) profileFields.social.twitter = twitter;
-    if (facebook) profileFields.social.facebook = facebook;
-    if (linkedin) profileFields.social.linkedin = linkedin;
-    if (instagram) profileFields.social.instagram = instagram;    const profileFields = {};
-    profileFields.user = req.user.id;
-    if (company) profileFields.company = company;
-    if (website) profileFields.website = website;
-    if (location) profileFields.location = location;
-    if (bio) profileFields.bio = bio;
-    if (status) profileFields.status = status;
-    if (githubusername) profileFields.githubusername = githubusername;
-    if (skills) {
-      profileFields.skills = skills.split(',').map(skill => skill.trim());
-    }
-
-    // Build social object
-    profileFields.social = {};
-    if (youtube) profileFields.social.youtube = youtube;
-    if (twitter) profileFields.social.twitter = twitter;
-    if (facebook) profileFields.social.facebook = facebook;
-    if (linkedin) profileFields.social.linkedin = linkedin;
-    if (instagram) profileFields.social.instagram = instagram;
-
-    //Build profile object
-   }
 );
-            
+
 
 module.exports = router;
