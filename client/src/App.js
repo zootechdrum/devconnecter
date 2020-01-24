4d86ccb51f9c058cc2fc830ea4c824ee48e803e3
Cesar Gomez <zootechdrum@gmail.com>
2020-01-24 12:45:01 -0800

    fixed syntax to despay component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React, { Fragment } from 'react';
 import './App.css';
-import { Fragment } from '../node_modules/@types/react';
 
-const App = () => {
 
+
+
+
+const App = () => (
   <Fragment>
     <h1>App</h1>
   </Fragment>
-
-};
+);
 
 
 export default App;
