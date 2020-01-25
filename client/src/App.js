7e5922550d801d296c358ba82fc0f58f06f0a98e
Cesar Gomez <zootechdrum@gmail.com>
2020-01-24 23:10:59 -0800

    links in navbar work

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react';
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 
@@ -8,10 +11,18 @@ import './App.css';
 
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className="container">
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 
